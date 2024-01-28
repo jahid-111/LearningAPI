04b6d7e3a0a9d44d4f318215fd4eebd295c0e26c
Mohd. Jahidul Islam <jahidjob5@outlook.com>
2024-01-28 05:43:16 +0300

    ClickToDisplay

--- json.js
+++ json.js
@@ -1,6 +1,6 @@
 // console.log("json.js")
 
-const dataLink = 'https://jsonplaceholder.typicode.com/todos/1'
+const dataLink = 'https://jsonplaceholder.typicode.com/users'
 
 fetch(dataLink)
 .then(res => res.json())
@@ -13,6 +13,27 @@ function dataLoad(){
 
     fetch(dataLink)
     .then(res => res.json())
-    .then(json => console.log(json))
+    .then(json => user(json))
 
 }
+// dataLoad()
+
+const dataSection = document.getElementById('data-section');
+
+
+// console.log(dataSection)
+
+function user(datas){
+    // console.log(datas)
+    for(data of datas){
+      
+    const li = document.createElement('li')
+
+    li.innerText = data.name;
+    dataSection.appendChild(li)
+        // console.log(li)
+    }
+
+    
+
+} 
