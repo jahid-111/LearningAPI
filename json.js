ba30709e968857e55f620d22b9309507b00a1cc1
Mohd. Jahidul Islam <jahidjob5@outlook.com>
2024-01-29 02:32:55 +0300

    BasicAPI_LoadData

--- json.js
+++ json.js
@@ -4,7 +4,7 @@ const dataLink = 'https://jsonplaceholder.typicode.com/users'
 
 fetch(dataLink)
 .then(res => res.json())
-// .then(json => console.log(json))
+.then(json => console.log(json))
 
 
 
@@ -20,13 +20,11 @@ function dataLoad(){
 
 const dataSection = document.getElementById('data-section');
 
-
 // console.log(dataSection)
 
 function user(datas){
     // console.log(datas)
     for(data of datas){
-      
     const li = document.createElement('li')
 
     li.innerText = data.name;
@@ -34,6 +32,40 @@ function user(datas){
         // console.log(li)
     }
 
-    
-
 } 
+
+
+
+ const url = "https://jsonplaceholder.typicode.com/posts";
+        function getData(){
+            fetch(url)
+            .then(response => response.json() )
+            .then(reject => userData(reject))
+        }
+        const dataBox = document.querySelector(".dataBox")
+        // console.log(dataBox)
+
+    function userData(datas){
+        for(const data of datas ){
+            const div = document.createElement('div')
+            div.classList.add("post")
+                    div.innerHTML =` 
+                        <h4>User ID ${data.id} </h4>
+                        <h5>User Title : ${data.title} </div>                   
+                        <p>User Post : ${data.body}</p>
+                        `
+            dataBox.appendChild(div);
+            // console.log(data)
+        }
+    }
+ console.log(" API load From :" , url)
+
+
+
+
+
+
+
+
+
+
