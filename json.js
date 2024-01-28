c1931a2765e790efd7703710efac3a6bca7a53a8
Mohd. Jahidul Islam <jahidjob5@outlook.com>
2024-01-28 03:41:25 +0300

    onClickToView

--- json.js
+++ json.js
@@ -4,6 +4,15 @@ const dataLink = 'https://jsonplaceholder.typicode.com/todos/1'
 
 fetch(dataLink)
 .then(res => res.json())
-.then(json => console.log(json))
+// .then(json => console.log(json))
 
-    
+
+
+
+function dataLoad(){
+
+    fetch(dataLink)
+    .then(res => res.json())
+    .then(json => console.log(json))
+
+}
